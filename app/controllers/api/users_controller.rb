9427fd74d5d2093bafb441734150480cb225af53
Rhema <rhemaboyo@gmail.com>
2017-08-22 11:26:44 -0400

    write users controller methods and routes

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,35 @@
+class Api::UsersController < ApplicationController
+  before_action :require_logged_in!, only: [:update]
+  before_action :require_logged_out!, only: [:create]
+
+  def create
+    @user = @user.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = user.find(params[:id])
+    render :show
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    current_user.try(reset_session_token!)
+    session[:token] = nil
+  end
+
+  def login(user)
+    @current_user = user
+    session[:token] = user.reset_session_token!
+  end
+
+  def require_logged_in!
+    render 'api/session/new' unless logged_in?
+  end
+
+  def require_logged_out!
+    render `api/users/#{current_user.id}` if logged_in?
+  end
 end
