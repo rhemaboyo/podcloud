9e5e70444572574da55221ec5dd2b929fc682e91
Rhema <rhemaboyo@gmail.com>
2017-08-24 15:01:47 -0400

    finish styling nav bar

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -5,6 +5,7 @@ class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.logout = this.logout.bind(this);
+    this.preventDefault = this.preventDefault.bind(this);
   }
 
   logout(e) {
@@ -12,12 +13,22 @@ class NavBar extends React.Component {
     this.props.logout();
   }
 
+  preventDefault(e) {
+    e.preventDefault();
+  }
+
   render() {
     return(
       <div className='nav-container'>
         <nav className='nav'>
           <button onClick={this.logout}>Sign Out</button>
-          <button>{this.props.user.username}</button>
+          <button onClick={this.preventDefault}>{this.props.user.username}</button>
+          <button onClick={this.preventDefault}>Upload</button>
+          <img src='https://a-v2.sndcdn.com/assets/images/sc-icons/win8-2dc974a1.png'></img>
+          <button onClick={this.preventDefault}>Home</button>
+          <input onSubmit={this.preventDefault}
+            placeholder='Search...'
+            value=''/>
         </nav>
       </div>
     );
