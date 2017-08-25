56982ab44be1e38eaa9312881de237db9b5c7472
Rhema <rhemaboyo@gmail.com>
2017-08-25 13:07:44 -0400

    added file upload button to user page

--- frontend/components/user_page.jsx
+++ frontend/components/user_page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './nav_bar';
+import AvatarCropper from 'react-avatar-cropper';
 
 class UserPage extends React.Component {
   constructor(props) {
@@ -12,8 +13,15 @@ class UserPage extends React.Component {
         <NavBar user={this.props.user} logout={this.props.logout}/>
         <div className='user-image-container'>
           <img src={this.props.user.avatar} className='avatar'/>
-          <img src={this.props.user.banner_image} className='user-banner'/>
           <p>{this.props.user.username}</p>
+          <div className="banner-image-wrapper">
+            <img src={this.props.user.banner_image} className='user-banner'/>
+            <label htmlFor="banner-upload">
+              <i className="fa fa-camera" aria-hidden="true"></i>
+              Update Banner
+              <input type="file" id="files" className="hidden"/>
+            </label>
+          </div>
         </div>
         <div className='user-body'>
           <div className='track-container'></div>
@@ -22,5 +30,27 @@ class UserPage extends React.Component {
     );
   }
 }
+
+// class FileUpload extends React.Component{
+//
+//   handleFile(e) {
+//     var reader = new FileReader();
+//     var file = e.target.files[0];
+//
+//     if (!file) return;
+//
+//     reader.onload = function(img) {
+//       ReactDom.findDOMNode(this.refs.in).value = '';
+//       this.props.handleFileChange(img.target.result);
+//     }.bind(this);
+//     reader.readAsDataURL(file);
+//   }
+//
+//   render() {
+//     return (
+//       <input ref="in" type="file" accept="image/*" onChange={this.handleFile} />
+//     );
+//   }
+// }
 
 export default UserPage;
