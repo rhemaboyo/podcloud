d23306ac0ed2aa04f09c366da44c557e3298c3d6
Rhema <rhemaboyo@gmail.com>
2017-08-28 12:13:09 -0400

    style play pause

--- frontend/components/episode_index_item.jsx
+++ frontend/components/episode_index_item.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 class EpisodeIndexItem extends React.Component {
   render() {
+    const playPauseButton = //this.props.playing === this.props.episode ?
+      <i className="fa fa-pause"
+         id='playPause'
+         aria-hidden="true"></i>;
+        // <i className="fa fa-play"
+        //    id='playPause'
+        //    aria-hidden="true"></i>;
     return(
       <li>
-        <div className='episode'>
+        <div className='episode-container'>
+          {playPauseButton}
           <img className='episode' src={`${this.props.episode.image_url}`}/>
-          <i class="fa fa-play" aria-hidden="true"></i>
         </div>
       </li>
     );
