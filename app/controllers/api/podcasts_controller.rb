d50159db796f7928ad98bfabdb1d3badf56f6721
Rhema <rhemaboyo@gmail.com>
2017-11-08 19:38:20 -0500

    fix bugs with create podcast method

--- app/controllers/api/podcasts_controller.rb
+++ app/controllers/api/podcasts_controller.rb
@@ -1,7 +1,8 @@
 class Api::PodcastsController < ApplicationController
   def create
-    @podcast = Podcast.find_by(podcast_params)
+    @podcast = Podcast.find_by('itunes_id' => podcast_params['itunes_id'])
     if @podcast
+      @episodes = get_episodes(@podcast.feed_url)
       render :show
     else
       @podcast = Podcast.new(podcast_params)
